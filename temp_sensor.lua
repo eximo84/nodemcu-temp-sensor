cf0c5a584ae37f7aa54d6203b80b5ce85d296239
eximo84 <davejparkinson@gmail.com>
2017-02-02 16:54:33 +0000

    Removed Looping
    Loop code taken from here - https://odd-one-out.serek.eu/esp8266-nodemcu-dht22-custom-modules-firmware/
    
    Not really needed, was causing infinite loops if network was unavailable.  Instead code checks for a connection, if connected the values are printed to screen (soon to be MQTT server) and then the device goes into a deep sleep for 60 seconds
    
    If no connection the device sleeps for 60 seconds and then retries the connection.
    
    Will remove commented out codes when i am finished testing

--- temp_sensor.lua
+++ temp_sensor.lua
@@ -1,6 +1,19 @@
 --load settings
 dofile("settings.lua")
 
+-- Setup MQTT client and events
+m = mqtt.Client(client_id, 120, username, password)
+temperature = 0
+
+-- Connect to the wifi network
+wifi.setmode(wifi.STATION) 
+--wifi.setphymode(wifi_signal_mode)
+wifi.sta.config(wifi_SSID, wifi_password) 
+wifi.sta.connect()
+if client_ip ~= "" then
+    wifi.sta.setip({ip=client_ip,netmask=client_netmask,gateway=client_gateway})
+end
+
 function get_sensor_data()
     t = require("ds18b20")
 
@@ -25,12 +38,31 @@ function get_sensor_data()
 end
 
 --function loop()
-
-    get_sensor_data() 
-    print("Going to deep sleep for "..(time_between_sensor_readings/1000).." seconds")
-    --node.dsleep(time_between_sensor_readings*1000)  
+    if wifi.sta.status() == 5 then
+        -- Stop the loop
+--        tmr.stop(0)
+--        m:connect( mqtt_broker_ip , mqtt_broker_port, 0, function(conn)
+--            print("Connected to MQTT")
+--            print("  IP: ".. mqtt_broker_ip)
+--            print("  Port: ".. mqtt_broker_port)
+--            print("  Client ID: ".. mqtt_client_id)
+--            print("  Username: ".. mqtt_username)
+            -- Get sensor data
+            get_sensor_data() 
+--            m:publish("ESP8266/temperature",temperature, 0, 0, function(conn)
+                print("Going to deep sleep for "..(time_between_sensor_readings/1000).." seconds")
+                node.dsleep(time_between_sensor_readings*1000)             
+--            end)          
+--        end)
+    else
+        print("Network not available")
+        print("Going to deep sleep for "..(time_between_sensor_readings/1000).." seconds")
+        node.dsleep(time_between_sensor_readings*1000) 
+    end  
 --end           
 
 --tmr.alarm(0, 100, 1, function() loop() end)
 
+-- Watchdog loop, will force deep sleep the operation somehow takes to long
+--tmr.alarm(1,4000,1,function() node.dsleep(time_between_sensor_readings*1000) end)
     
