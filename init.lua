9eed36ca6051c8601f1c96f6b1394cabd0e03252
eximo84 <davejparkinson@gmail.com>
2017-02-02 11:43:58 +0000

    Create init.lua
    Based on https://github.com/ckuehnel/NodeMCU-applications/blob/master/init.lua

---
+++ init.lua
@@ -0,0 +1,30 @@
+--load settings
+dofile("settings.lua")
+
+function startup()
+    if file.open("init.lua") == nil then
+      print("init.lua deleted")
+    else
+      print("Running")
+      file.close("init.lua")
+--      dofile("temp-sensor.lua")
+    end
+end
+
+--init.lua
+wifi.sta.disconnect()
+print("set up wifi mode")
+wifi.setmode(wifi.STATION)
+wifi.sta.config(SSID,PASSWORD,0)
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function() 
+    if wifi.sta.getip()== nil then 
+        print("IP unavaiable, Waiting...") 
+    else 
+        tmr.stop(1)
+        print("Config done, IP is "..wifi.sta.getip())
+        print("You have 5 seconds to abort Startup")
+        print("Waiting...")
+        tmr.alarm(0,5000,0,startup)
+    end 
+ end)
