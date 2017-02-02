24483c617ed1162f1fb1a5aa18476a32af984e9c
eximo84 <davejparkinson@gmail.com>
2017-02-02 16:51:48 +0000

    Streamlined Init.lua
    Removed network connection, init.lua now only starts the lua script for what ever application you are running.  Has the ability to break out of the script by typing "abort=true"

--- init.lua
+++ init.lua
@@ -1,30 +1,13 @@
---load settings
-dofile("settings.lua")
+--init.lua
 
 function startup()
     if abort == true then
-        print('startup aborted')
+        print('Startup Aborted')
         return
         end
-    print('in startup')
-    -- dofile("temp_sensor.lua")
+    print('Starting Application...')
+    dofile("temp_sensor.lua")
 end
-
---init.lua
-wifi.sta.disconnect()
-print("set up wifi mode")
-wifi.setmode(wifi.STATION)
-wifi.setphymode(wifi_signal_mode)
-wifi.sta.config(wifi_SSID,wifi_password,0)
-wifi.sta.connect()
-tmr.alarm(1, 1000, 1, function() 
-    if wifi.sta.getip()== nil then 
-        print("IP unavaiable, Waiting...") 
-    else 
-        tmr.stop(1)
-        print("Config done, IP is "..wifi.sta.getip())
-    end 
- end)
 
 abort = false
 tmr.alarm(0,5000,0,startup)
